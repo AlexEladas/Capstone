476901607e475aa097d516d6c4bf34f94186e591
alexeladas <alexeladas@gmail.com>
2018-03-06 13:07:08 -0500

    Made gauss file easier to import

--- gauss.py
+++ gauss.py
@@ -3,39 +3,44 @@ import math
 import sys
 #import pyttsx3
 import os
-pri = 1.0/7.0
-test = sys.argv[1:]
-print (pri)
 
-def gauss(mean, variance, feature):
-    p = ((1/math.sqrt(2*math.pi*variance))*math.exp(-((feature - mean)**2)/(2*variance)))
-    return p
+def main(arg):
+    pri = 1.0/7.0
+    test = arg
+    print (pri)
 
-with open('mean.csv', 'r') as f:
-    reader = csv.reader(f)
-    m = list(reader)
+    def gauss(mean, variance, feature):
+        p = ((1/math.sqrt(2*math.pi*variance))*math.exp(-((feature - mean)**2)/(2*variance)))
+        return p
 
-mean_list = [lst[1:] for lst in m]
-print (mean_list)
-with open('variance.csv', 'r') as f:
-    reader = csv.reader(f)
-    v = list(reader)
+    with open('mean.csv', 'r') as f:
+        reader = csv.reader(f)
+        m = list(reader)
 
-variance_list = [lst[1:] for lst in v]
-print (variance_list)
-final = []
-finald = {}
-letters = ['a','b','c','d','e','water','help']
-for i in range(len(mean_list)):
-    score = 1.0
-    for mean,variance, value in zip(mean_list[i],variance_list[i],test):
-        score *= gauss(float(mean),float(variance),float(value))
-    score *= pri
-    print("Class "+letters[i] +": " + str(score))
-    finald[score] = letters[i]
-    final.append(score)
-#engine = pyttsx3.init()
-#engine.say(finald[max(final)])
-os.system("pico2wave -w lookdave.wav {b} && aplay lookdave.wav".format(b= finald[max(final)]))
-os.system("rm lookdave.wav")
-#engine.runAndWait()
+    mean_list = [lst[1:] for lst in m]
+    print (mean_list)
+    with open('variance.csv', 'r') as f:
+        reader = csv.reader(f)
+        v = list(reader)
+
+    variance_list = [lst[1:] for lst in v]
+    print (variance_list)
+    final = []
+    finald = {}
+    letters = ['a','b','c','d','e','water','help']
+    for i in range(len(mean_list)):
+        score = 1.0
+        for mean,variance, value in zip(mean_list[i],variance_list[i],test):
+            score *= gauss(float(mean),float(variance),float(value))
+        score *= pri
+        print("Class "+letters[i] +": " + str(score))
+        finald[score] = letters[i]
+        final.append(score)
+    #engine = pyttsx3.init()
+    #engine.say(finald[max(final)])
+    os.system("pico2wave -w lookdave.wav {b} && aplay lookdave.wav".format(b= finald[max(final)]))
+    os.system("rm lookdave.wav")
+    #engine.runAndWait()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
