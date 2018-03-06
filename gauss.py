1e65c62cb86a0231b53bb9c155619851783c6c78
alexeladas <alexeladas@gmail.com>
2018-03-05 23:46:46 -0500

    Updated algorithm

--- gauss.py
+++ gauss.py
@@ -1,14 +1,14 @@
 import csv
 import math
 import sys
-#import pyttsx3
+import pyttsx3
 
-pri = 0.2
-test = sys.argv[1:]
-
+pri = 1.0/7.0
+test = sys.argv[1]
+print (pri)
 
 def gauss(mean, variance, feature):
-    p = (1/math.sqrt(2*math.pi*variance))*math.exp((-(feature - mean)**2)/2*variance)
+    p = ((1/math.sqrt(2*math.pi*variance))*math.exp(-((feature - mean)**2)/(2*variance)))
     return p
 
 with open('mean.csv', 'r') as f:
@@ -16,25 +16,24 @@ with open('mean.csv', 'r') as f:
     m = list(reader)
 
 mean_list = [lst[1:] for lst in m]
-
+print (mean_list)
 with open('variance.csv', 'r') as f:
     reader = csv.reader(f)
     v = list(reader)
 
 variance_list = [lst[1:] for lst in v]
+print (variance_list)
 final = []
 finald = {}
-letters = ['a','b','c','d','e']
+letters = ['a','b','c','d','e','water','help']
 for i in range(len(mean_list)):
-    score = 1
+    score = 1.0
     for mean,variance, value in zip(mean_list[i],variance_list[i],test):
         score *= gauss(float(mean),float(variance),float(value))
     score *= pri
     print("Class "+letters[i] +": " + str(score))
     finald[score] = letters[i]
     final.append(score)
-#engine = pyttsx3.init()
-#engine.say(finald[max(final)])
-#engine.say("Hungry")
-#engine.say("Water")
-#engine.runAndWait()
+engine = pyttsx3.init()
+engine.say(finald[max(final)])
+engine.runAndWait()
